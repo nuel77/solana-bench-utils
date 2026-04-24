dfabd481e97ba526a44958a266700168cf92ba88
nuel77 <emmanuel.thomas237@gmail.com>
2026-04-24 11:04:27 +0000

    use delay

--- nonce-bench/src/main.rs
+++ nonce-bench/src/main.rs
@@ -206,6 +206,7 @@ async fn run_iteration(
         let tip_key = *tip;
         handles.push(tokio::spawn(async move {
             if tip_key == pubkey!("astrazznxsGUhWShqgNtAdfrzP2G83DzcWVJDxwV9bF"){
+                info!("sleeping start");
                 tokio::time::sleep(Duration::from_millis(6)).await;
                 info!("sleeping done");
             }
